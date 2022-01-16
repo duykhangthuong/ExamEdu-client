8c14e887bd5605fb18b132cea7c8a3c7a4bb2b96
David150022 <doanngocminh2@gmail.com>
2022-01-16 11:09:02 +0700

    Added routing for sprint 1

---
+++ src/pages/Administrator/AccountList.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const AccountList = () => {
+    return <div></div>;
+};
+
+export default AccountList;
---
+++ src/routes/StudentRoutes.js
@@ -0,0 +1,22 @@
+import ExamSchedule from "pages/Student/ExamSchedule";
+
+import MarkReport from "pages/Student/MarkReport";
+import React from "react";
+import { Route } from "react-router-dom";
+import { Switch } from "react-router-dom";
+
+const StudentRoutes = () => {
+    return (
+        <Switch>
+            <Route path="/student/ExamSchedule">
+                <ExamSchedule />
+            </Route>
+
+            <Route path="/student/MarkReport">
+                <MarkReport />
+            </Route>
+        </Switch>
+    );
+};
+
+export default StudentRoutes;
