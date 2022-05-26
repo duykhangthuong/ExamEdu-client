701846e756e076e86ff98b3a0e4686c62c075ee5
unknown <tieuanhtho@gmail.com>
2022-05-26 15:36:45 +0700

    them chuc nang tat mic, gui metadata

--- src/pages/Student/StudentCall.js
+++ src/pages/Student/StudentCall.js
@@ -1,18 +1,31 @@
 import Peer from "peerjs";
-
+import { useEffect, useState } from "react";
+import { useSelector } from "react-redux";
 const StudentCall = () => {
-
     const roomID = "test";
+    const user = useSelector((state) => state.user);
+    var local_stream;
+
+    const changeLocalVideoState = () => {
+        local_stream.getVideoTracks()[0].enabled = !local_stream.getVideoTracks()[0].enabled;
+        console.log(user)
+    }
+    const changeLocalAudioState = () => {
+        local_stream.getAudioTracks()[0].enabled = !local_stream.getAudioTracks()[0].enabled;
+    }
 
     var peer = new Peer();
     peer.on('open', (id) => {
         console.log("Connected with Id: " + id)
         navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
             let video = document.getElementById("local-video");
+            local_stream = stream;
             video.srcObject = stream;
             video.muted = true;
             video.play();
-            let call = peer.call(roomID, stream)
+            let call = peer.call(roomID, stream, {
+                 metadata: { userEmail: user.email,
+                            userFullname: user.fullname } });
             let count = 0;
             call.on('stream', (stream) => {
                 count++;
@@ -25,12 +38,24 @@ const StudentCall = () => {
                 video.play();
             })
         })
+
     })
+
+    useEffect(() => {
+        window.addEventListener("beforeunload", (ev) => {
+            ev.preventDefault();
+            return ev.returnValue = 'Are you sure you want to close?';
+        });
+    }, []);
+
+
 
     return (
         <div>
             <video id="local-video"></video>
             <div id="remote-video"></div>
+            <button onClick={() => changeLocalVideoState()}>video state</button>
+            <button onClick={() => changeLocalAudioState()}>audio</button>
         </div>
     );
 }
