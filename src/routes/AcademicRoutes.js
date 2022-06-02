f1fab75bbe9fe4049773ad57a01de2c7accb7af4
Kha Minh Ho <hokhaminh97@gmail.com>
2022-06-02 14:57:45 +0000

    Exam list of Academic Department, add button in Exam Detail

--- src/routes/AcademicRoutes.js
+++ src/routes/AcademicRoutes.js
@@ -8,6 +8,8 @@ import ModuleList from "pages/AcademicDepartment/ModuleList";
 import ClassList from "pages/AcademicDepartment/ClassList";
 import ClassDetail from "pages/AcademicDepartment/ClassDetail";
 import UpdateExam from "pages/AcademicDepartment/UpdateExam";
+import ExamList from "pages/AcademicDepartment/ExamList";
+import ExamDetail from "pages/AcademicDepartment/ExamDetail";
 const AcademicRoutes = () => {
     const { redirect, path } = useUserAuthorization("academicdepartment");
 
@@ -17,8 +19,6 @@ const AcademicRoutes = () => {
             <HorizontalNavBar />
             <VerticalNavBar VerticalNavbarData={DataAcademic} />
             <Switch>
-            
-
                 <Route path="/AcademicDepartment/module/list" exact>
                     <ModuleList />
                 </Route>
@@ -26,8 +26,14 @@ const AcademicRoutes = () => {
                 <Route path="/AcademicDepartment/class/" exact>
                     <ClassList />
                 </Route>
+                <Route path="/AcademicDepartment/exam" exact>
+                    <ExamList />
+                </Route>
                 <Route path="/AcademicDepartment/class/:classId" exact>
                     <ClassDetail />
+                </Route>
+                <Route path="/AcademicDepartment/exam/:examId" exact>
+                    <ExamDetail />
                 </Route>
 
                 {/* <Route path="/academic/class/create" exact></Route>
@@ -37,7 +43,10 @@ const AcademicRoutes = () => {
                 <Route path="/academic/exam/create" exact></Route>
                 <Route path="/academic/exam/update" exact></Route> */}
 
-                <Route path="/AcademicDepartment/exam/update/info/:examId" exact>
+                <Route
+                    path="/AcademicDepartment/exam/update/info/:examId"
+                    exact
+                >
                     <UpdateExam />
                 </Route>
 
