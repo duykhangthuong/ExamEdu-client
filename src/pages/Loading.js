39b849111d908a4fa146ae8a84c7e0d2a1342a72
Kha Minh Ho <hokhaminh97@gmail.com>
2022-06-09 03:08:42 +0000

    Done Exam Detail

--- src/pages/Loading.js
+++ src/pages/Loading.js
@@ -1,9 +1,12 @@
 import Wrapper from "components/Wrapper";
 import React from "react";
 import "styles/Loading.css";
-const Loading = () => {
+const Loading = ({ className, style = {} }) => {
     return (
-        <Wrapper className="d-flex justtify-content-center align-items-center">
+        <Wrapper
+            className={`d-flex justtify-content-center align-items-center ${className}`}
+            style={style}
+        >
             <div className="loader"></div>;
         </Wrapper>
     );
